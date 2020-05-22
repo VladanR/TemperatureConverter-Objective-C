327946750bfe871e2513c0fb798e1b6a2917585e
VladanR <x3m.rocker.035@gmail.com>
2020-05-23 01:44:28 +0200

    Add Celsius to Fahrenheit conversion and wrap up image switching when temp changes

--- TemperatureConverter/ViewController.m
+++ TemperatureConverter/ViewController.m
@@ -13,7 +13,8 @@
 @end
 
 @implementation ViewController
-
+double tempFahrenheit;
+double tempCelsius;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -38,11 +39,57 @@
     
     if (self.switchConversion.selectedSegmentIndex == 0) {
         
-        double tempFahrenheit = [self.textInputField.text doubleValue];
-        double tempCelsius = (tempFahrenheit - 32)/1.8;
-        self.outputLabel.text = [NSString stringWithFormat:@"%4.3f", tempCelsius];
+        tempFahrenheit = [self.textInputField.text doubleValue];
+        tempCelsius = (tempFahrenheit - 32)/1.8;
+        self.outputLabel.text = [NSString stringWithFormat:@"%4.2f Celsius", tempCelsius];
         
+        if (tempCelsius > 120) {
+          self.temperatureImage.image = [UIImage imageNamed:@"Temp9"];
+        } else if (tempFahrenheit > 100) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp8"];
+        } else if (tempFahrenheit > 80) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp7"];
+        } else if (tempFahrenheit > 60) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp6"];
+        } else if (tempFahrenheit > 40) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp5"];
+        } else if (tempFahrenheit > 20) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp4"];
+        } else if (tempFahrenheit > 0) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp3"];
+        } else if (tempFahrenheit > -20) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp2"];
+        } else if (tempFahrenheit < -20) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp1"];
+        }
     }
+    
+    if (self.switchConversion.selectedSegmentIndex == 1) {
+        tempCelsius = [self.textInputField.text doubleValue];
+        tempFahrenheit = tempCelsius * 1.8 + 32;
+        self.outputLabel.text = [NSString stringWithFormat:@"%4.2f Fahrenheit", tempFahrenheit];
+        
+        if (tempFahrenheit > 180) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp9"];
+        } else if (tempFahrenheit > 160) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp8"];
+        } else if (tempFahrenheit > 140) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp7"];
+        } else if (tempFahrenheit > 120) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp6"];
+        } else if (tempFahrenheit > 100) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp5"];
+        } else if (tempFahrenheit > 80) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp4"];
+        } else if (tempFahrenheit > 60) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp3"];
+        } else if (tempFahrenheit > 40) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp2"];
+        } else if (tempFahrenheit < 40) {
+            self.temperatureImage.image = [UIImage imageNamed:@"Temp1"];
+        }
+    }
+    [self resignFirstResponder];
     
 }
 
